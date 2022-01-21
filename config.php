b8175c0314398ca8f21a164b831a7b5acbd8a947
Howard Yeend <howard.yeend@gmail.com>
2022-01-21 15:03:47 +0000

    Moved password conf into gitignored file

--- config.php
+++ config.php
@@ -123,26 +123,9 @@ define('AUTOLINK_PAGE_TITLES', false);
 // Is a password required to access this wiki?
 
 define('REQUIRE_PASSWORD', false);
-
-// W2_PASSWORD
-//
-// The password for the wiki, if REQUIRE_PASSWORD is true
-// Replace 'secret' with your password to set your password.
-
-define('W2_PASSWORD', 'secret');
-
-// W2_PASSWORD_HASH
-//
-// Alternate (more secure) password storage.
-// To use a hashed password, Comment out the W2_PASSWORD definition above and uncomment
-// this one, using the result of sha1('your_password') as the value.
-//
-// In Mac OS X, you can do this from the Terminal:   
-//     echo -n 'your_password' | openssl sha1
-//
-// define('W2_PASSWORD_HASH', 'e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4');
-
-define('W2_PASSWORD_HASH', '');
+if (REQUIRE_PASSWORD) {
+  include_once "password.php";
+}
 
 // allowedIPs
 //
---
+++ sample_password.php
@@ -0,0 +1,21 @@
+<?php
+
+// Set REQUIRE_PASSWORD to true and save this file as password.php in order to use these features.
+
+// W2_PASSWORD
+//
+// The password for the wiki, if REQUIRE_PASSWORD is true
+// Replace 'secret' with your password to set your password.
+
+// define('W2_PASSWORD', 'secret');
+
+// W2_PASSWORD_HASH
+//
+// Alternate (more secure) password storage.
+// To use a hashed password, Comment out the W2_PASSWORD definition above and uncomment
+// this one, using the result of sha1('your_password') as the value.
+//
+// In Mac OS X, you can do this from the Terminal:   
+//     echo -n 'your_password' | openssl sha1
+//
+// define('W2_PASSWORD_HASH', 'e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4');
